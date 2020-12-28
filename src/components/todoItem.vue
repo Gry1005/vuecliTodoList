48c57ccb3cce8691840badccb9c080f61ee5e293
Gry1005 <runyuguan@outlook.com>
2020-12-27 17:25:16 -0800

    first commit

---
+++ src/components/todoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class='item' @click="handle_click">{{content}}</li>
+</template>
+
+<script>
+export default {
+  //接收父组件的传值
+  props:['content','index'],
+  methods:{
+    handle_click(){
+      //delete
+      //把数据传给父组件
+      this.$emit('delete',this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.item{
+  color:green;
+}
+</style>
---
+++ src/todoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input class="item" v-model="inputValue"/>
+    <button @click="handle_submit">提交</button>
+    <ul>
+      <todo-item v-for="(item,index) of list" :key="index" :content="item" :index="index" @delete="handle_delete"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+ import todoItem from './components/todoItem'
+
+export default {
+  components:{'todo-item':todoItem},
+  //data变为一个函数
+  data()
+  {
+    return{
+      inputValue:'',
+      list:[]
+    }
+  },
+  methods:{
+    handle_submit(){
+      this.list.push(this.inputValue);
+      this.inputValue='';
+    },
+    handle_delete(index){
+      this.list.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
